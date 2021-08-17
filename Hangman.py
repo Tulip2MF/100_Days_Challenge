664b18e3c8f5f0761e5eb7b6ddee8c914771f0c5
Tulip2MF <tulip2mf@gmail.com>
2021-08-17 23:49:01 +0530

    Test Commit

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,84 @@
 import random
-wordList=['Apple', 'Ball', 'Cat']
-randomWord = wordList[randint(0,2)].lower
-print(randomWord)
+hangman = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+wordList = ['apple', 'ball', 'cat']
+randomWord = wordList[random.randint(0, 2)].lower()
+print("_"*len(randomWord))
+
+outputWord = []
+for letter in range(len(randomWord)):
+    outputWord.append("_")
+k=0
+
+for j in range(11):
+    guessLetter = input("Guess a letter: ").lower()
+    wrongNumber = 0
+    for i in range(len(randomWord)):
+
+        if randomWord[i]==guessLetter:
+
+            #print(outputWord)
+
+            outputWord[i]=guessLetter
+            wrongNumber = 0
+        else:
+            wrongNumber +=1
+            print(j)
+    if wrongNumber==len(randomWord):
+        print(hangman[k])
+        k+=1
+
+    display =''.join(outputWord)
+    print(display)
+    if display == randomWord:
+        print("You guessed it correct.")
+        break
+print(f"The correct word is {randomWord}")
