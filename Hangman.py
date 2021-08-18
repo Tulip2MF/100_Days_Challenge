02153a40d8d9b239f79d83782e5fcd326108cfca
Tulip2MF <tulip2mf@gmail.com>
2021-08-18 11:24:50 +0530

    Test Commit

--- Hangman.py
+++ Hangman.py
@@ -1,84 +1,54 @@
 import random
-hangman = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
-wordList = ['apple', 'ball', 'cat']
-randomWord = wordList[random.randint(0, 2)].lower()
-print("_"*len(randomWord))
-
-outputWord = []
-for letter in range(len(randomWord)):
-    outputWord.append("_")
+import hangmanWords
+import hangmanArt
 k=0
+while True:
+    print(hangmanArt.logo)
+    wordList = hangmanWords.word_list
+    randomWord = random.choice(wordList)
+    print("_"*len(randomWord))
+    print(randomWord)
+    outputWord = []
+    for letter in range(len(randomWord)):
+        outputWord.append("_")
 
-for j in range(11):
-    guessLetter = input("Guess a letter: ").lower()
-    wrongNumber = 0
-    for i in range(len(randomWord)):
 
-        if randomWord[i]==guessLetter:
-
-            #print(outputWord)
-
-            outputWord[i]=guessLetter
-            wrongNumber = 0
-        else:
-            wrongNumber +=1
+    for j in range(7):
+        guessLetter = input("Guess a letter: ").lower()
+        wrongNumber = 0
+        for i in range(len(randomWord)):
             print(j)
-    if wrongNumber==len(randomWord):
-        print(hangman[k])
-        k+=1
+            if randomWord[i]==guessLetter:
+                if j==0:
+                    pass
+                else:
+                    j=j=j-1
+                print(j,"loop")
+                #print(outputWord)
+                if guessLetter in outputWord:
+                    print(f"You have entered the letter '{guessLetter}' already")
+                outputWord[i]=guessLetter
+                wrongNumber = 0
 
-    display =''.join(outputWord)
-    print(display)
-    if display == randomWord:
-        print("You guessed it correct.")
+            else:
+                wrongNumber +=1
+                j=k
+
+
+        if wrongNumber==len(randomWord):
+            print(f"'{guessLetter}' is not a letter in the word" )
+            print(k, "K")
+            print(j,"j")
+            print(hangmanArt.hangman[k]+"\n\n")
+            k=k+1
+
+
+        display =''.join(outputWord)
+        print(display)
+        if display == randomWord:
+            print("You guessed it correct.")
+            break
+    print(f"The correct word is **{randomWord}**")
+    repeatGame = input("Do you want to continue the game?\nPress 'y' for yes and 'n' for no").lower()
+    if repeatGame == 'n':
         break
-print(f"The correct word is {randomWord}")
