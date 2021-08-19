c16335c3a0ec4c76128ca2f103e77b6ce4527e31
Tulip2MF <tulip2mf@gmail.com>
2021-08-19 13:38:12 +0530

    Test Commit

---
+++ Greetings.py
@@ -0,0 +1,10 @@
+def greetings(name,location):
+    print(f"Hello {name}?")
+    print(f"How is the weather in {location}?")
+
+user_name = input("What is your name? ")
+user_location = input("Where are you from? ")
+
+greetings(user_name, user_location)
+
+
