318c5f8b2f7da6131d64d0727ef358533b3eb847
Tulip2MF <tulip2mf@gmail.com>
2021-08-20 15:57:23 +0530

    Test Commit

--- Ceasar Cipher.py
+++ Ceasar Cipher.py
@@ -4,11 +4,12 @@ import ceasar_cipher_encoding
 action = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+output = ""
 message = []
 if action == "encode":
-    output = ceasar_cipher_encoding.encoding(text,shift)
+    output = ceasar_cipher_encoding.encoding(text, shift)
 elif action == "decode":
-    output = ceasar_cipher_decoding.decoding(text,shift)
+    output = ceasar_cipher_decoding.decoding(text, shift)
 else:
     print("Wrong input")
 
