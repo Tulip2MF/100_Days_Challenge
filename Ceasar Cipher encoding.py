be3d3d42788e4a73cdc8b580564daa4f365b8b6f
Tulip2MF <tulip2mf@gmail.com>
2021-08-20 12:52:30 +0530

    Test Commit

---
+++ Ceasar Cipher encoding.py
@@ -0,0 +1,14 @@
+import alphabets
+
+
+
+def encoding(message,number):
+    input_message_list=list(message)
+    for letter in input_message_list:
+        i=0
+        if letter == alphabets.alphabet:
+            i+=1
+            current_location = alphabets.alphabet.index().append
+            shifted_location =current_location+number
+    for location in shifted_location
+        print(alphabets.alphabet[location])
---
+++ Ceasar Cipher.py
@@ -0,0 +1,8 @@
+#action = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+import
+
+
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
